41479994ef5041a6e6912146275bc80d28cb1f52
Dongwoo Pae <Dongwoo@macbook-pro-2.local>
2019-04-25 19:00:47 -0700

    Initial Commit

---
+++ AboutMe (iOSPT1)/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AboutMe (iOSPT1)
+//
+//  Created by Dongwoo Pae on 4/25/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hi my name is johnny hicks")
+    }
+}
+
