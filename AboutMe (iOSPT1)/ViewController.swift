ea8aff4fc24ac4c0fcec86e6395e5221e8bb5221
Dongwoo Pae <Dongwoo@macbook-pro-2.local>
2019-04-25 19:49:03 -0700

    Add button to introduce Self

--- AboutMe (iOSPT1)/ViewController.swift
+++ AboutMe (iOSPT1)/ViewController.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var hobiesLabel: UILabel!
+    @IBOutlet weak var hobbiesLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hi my name is Dongwoo Pae")
         print("another one")
+    }
+    
+    @IBAction func introduceSelfButtonTapped(_ sender: UIButton) {
+        self.nameLabel.text = "Dongwoo Pae"
+        self.hobbiesLabel.text = "Basketball, Coding, Movies"
     }
 }
 
